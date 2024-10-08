f71966870320c83791941241c2beed64147c60d9
hon-nova <hon.tnguyen294@gmail.com>
2024-10-08 15:41:10 -0700

    added menu

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,11 +1,15 @@
-
+import '../../src/css/base.css'
 
 export const Menu =()=>{
 
 
    return (
       <div>
-         Menu lands here...
+         <div className="titleBar" style={{ background: 'linear-gradient(to right, lightgrey, #4dc8fa)' }}>
+            <h1 className="title">Menu - Orders</h1>
+            <type></type>
+            
+         </div>
       </div>
    )
 }
